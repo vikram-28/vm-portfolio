9ad09ce1d50675d41fd5f811ca6d5429fd7db1e2
vikram-28 <vikrammahadik28@gmail.com>
2025-12-29 00:05:04 +0530

    fixed navbar responsive issue

--- src/app/core/layout/header/header.component.ts
+++ src/app/core/layout/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component, HostListener } from '@angular/core';
 import { ThemeService } from '../../services/theme.service';
+import { Collapse } from 'bootstrap';
 
 @Component({
   selector: 'app-header',
@@ -20,9 +21,23 @@ toggleTheme() {
   this.themeService.toggleTheme();
 }
 
+isNavbarOpen = false;
+
+  toggleNavbar(navbarCollapse: HTMLElement) {
+    this.isNavbarOpen = !this.isNavbarOpen;
+
+    // Get instance or create one
+    const collapseInstance = Collapse.getInstance(navbarCollapse) || new Collapse(navbarCollapse, { toggle: false });
+
+    collapseInstance.toggle();
+  }
+
+
+
+
+
 // active state handled for navbar
 activeSection = 'about';
-
   ngAfterViewInit() {
     const sections = document.querySelectorAll('section');
 
